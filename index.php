0425e9d5a7a69d0611698e87328f9e0c0e8f6091
田斌 <tianbin@tianbindeMacBook-Pro.local>
2019-01-05 10:24:29 +0800

    测试

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+ini_set('display_errors'    , 'On');
+ini_set('output_buffering'  , 'Off');
+ini_set('opcache.enable'    , '0');
+error_reporting(E_ALL^E_NOTICE);
+function acrypt($string, $key, $operation = 'encode')
+{
+    echo $string;
+}
 echo '2018 hello world<br/>';
 
 echo phpinfo();
